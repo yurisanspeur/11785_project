ac89e749f0424900d84fb0c22903161a3ea27fc8
Yuri <rsanspeu@andrew.cmu.edu>
2023-07-28 15:34:57 -0700

    Adding lightning version.

---
+++ supervisedDL-SRVFdistances/lightning_imp.py
@@ -0,0 +1,144 @@
+import numpy as np
+from torch.utils.data import DataLoader, random_split, Dataset
+import torch
+import torch.nn as nn
+from pytorch_lightning import LightningModule, LightningDataModule
+from pytorch_lightning.loggers import WandbLogger
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+from torch.optim.lr_scheduler import ReduceLROnPlateau, CyclicLR, CosineAnnealingLR
+from matplotlib import pyplot as plt
+import os
+import math
+wandb_logger = WandbLogger(project="Oblika", name="cyclic_pad_model_more_patient")
+#data = np.load(
+#    "massive_training_data_distances.pickle",
+#    allow_pickle=True,
+#)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_float32_matmul_precision('high')
+torch.manual_seed(0)
+#c1_xs = []
+#c1_gs = []
+#c2_xs = []
+#c2_gs = []
+#dists = []
+#for i in range(len(data)): # We will write this data once to disk at the sample level (a pair of shape and its corresponding distance)
+    # if len(np.array(data[i]['c1']['G'])) == 99 and len(np.array(data[i]['c2']['G'])) == 99:
+#    c1_xs.append(np.array(data[i]["c1"]["x"]))
+    # c1_gs.append(np.array(data[i]['c1']['G']))
+#    c2_xs.append(np.array(data[i]["c2"]["x"]))
+    # c2_gs.append(np.array(data[i]['c2']['G']))
+#    dists.append(np.array(data[i]["dist"]))
+#    X = torch.cat((torch.Tensor(data[i]["c1"]["x"]), torch.Tensor(data[i]["c2"]["x"])), dim=1) # This should be (100,4)
+#    dist = torch.Tensor([data[i]["dist"]])
+#    torch.save((X,dist), f"Fab_data/data_{i}.pt")
+    # c1_xs = np.array(c1_xs)
+    # c1_gs = np.array(c1_gs)
+    # c2_xs = np.array(c2_xs)
+    # c2_gs = np.array(c2_gs)
+#cxy_data = np.concatenate((c1_xs, c2_xs), axis=2)
+#dists = torch.FloatTensor(np.array([dists]).T)
+
+#cxy_data = torch.FloatTensor(cxy_data)  # cast as a torch.float32 tensor
+#label = (
+#    dists.squeeze()
+#)  # cast as torch.float32 for compatility with layer kernels and should have only batch_size dimension
+
+#generator = torch.Generator()
+#generator.manual_seed(0)
+class ShapeDataset(torch.utils.data.Dataset):
+    def __init__(self, path='Fab_data'):
+        self.path = path
+
+    def __len__(self):
+        return len(os.listdir(self.path))
+
+    def __getitem__(self, ind):
+        X, dist = torch.load(f"Fab_data/data_{ind}.pt")
+        return X, dist
+
+
+#class ShapeTestDataset(torch.utils.data.Dataset):
+#    def __init__(self, feature_data, label):
+#        self.feature_data = feature_data
+#        self.label = label
+#
+#    def __len__(self):
+#        return len(self.feature_data)
+#
+#    def __getitem__(self, ind):
+#        return self.feature_data[ind], self.label[ind]
+cxy_data = ShapeDataset(path="Fab_data")
+len_data = len(cxy_data)
+train_len = int(0.8 * len_data)
+val_len = int(0.1 * len_data)
+test_len = len_data - train_len - val_len
+train_data, val_data, test_data = torch.utils.data.random_split(
+    cxy_data, [train_len, val_len, test_len]
+)
+
+
+BATCH_SIZE = 128
+
+#train_loader = torch.utils.data.DataLoader(
+#    train_data, num_workers=8, batch_size=BATCH_SIZE, pin_memory=True, shuffle=True
+#)
+#val_loader = torch.utils.data.DataLoader(
+#    val_data, num_workers=8, batch_size=BATCH_SIZE, pin_memory=True, shuffle=False
+#)
+#test_loader = torch.utils.data.DataLoader(
+#    test_data, num_workers=8, batch_size=BATCH_SIZE, pin_memory=True, shuffle=False
+#)
+class MyDataModule(LightningDataModule):
+    def __init__(
+        self,
+        batch_size,
+        train_set,                                                                          
+        val_set,                                                                            
+        test_set,                                                                           
+        train_collate_fn=None,                                                              
+        val_collate_fn=None,                                                                
+        test_collate_fn=None,                                                               
+    ):                                                                                                                                                                                   
+        self.train_set = train_set                                                          
+        self.val_set = val_set                                                              
+        self.test_set = test_set                                                            
+        self.batch_size = batch_size                                                        
+        self.train_collate_fn = train_collate_fn                                            
+        self.val_collate_fn = val_collate_fn                                                
+        self.test_collate_fn = test_collate_fn                                              
+                                                                                            
+    def train_dataloader(self):                                                                                                                                                          
+        return DataLoader(                                                                  
+            self.train_set,                                                                 
+            batch_size=self.batch_size,                                                                                                                                                  
+            shuffle=True,                                                                   
+            pin_memory=True,                                                                
+            num_workers=8,                                                                                                                                                               
+            persistent_workers=True,                                                        
+            collate_fn=self.train_collate_fn,                                               
+        )                                                                                   
+                                                                                            
+    def val_dataloader(self):                                                               
+        return DataLoader(                                                                  
+            self.val_set,                                                                                                                                                                
+            batch_size=self.batch_size,                                                     
+            shuffle=False,                                                                                                                                                               
+            pin_memory=True,                                                                
+            persistent_workers=True,                                                        
+            num_workers=8,                                                                  
+            collate_fn=self.val_collate_fn,                                                 
+        )                                                                                   
+                                                                                            
+    def test_dataloader(self):                                                              
+        return DataLoader(                                                                  
+            self.test_set,                                                                  
+            batch_size=self.batch_size,                                                     
+            shuffle=False,                                                                                                                                                               
+            pin_memory=True,                                                                
+            persistent_workers=True,                                                        
+            num_workers=8,
+            collate_fn=self.test_collate_fn,
+        )
